8d7eea806ed181d699b1690be89ad333f670fadd
Rabindra Dahal <rabindradahal2076@gmail.com>
2024-07-26 12:41:41 +0545

    added context data'

--- base/views.py
+++ base/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+data = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Lets learn Phoenix'},
+    {'id': 3, 'name': 'Lets learn Web Development'},
+    ]
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': data})
 
 def rooms(request):
     return render(request, 'rooms.html')
