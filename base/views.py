73add427305cc75382dbf996a5eba81b8dad14a0
Rabindra Dahal <rabindradahal2076@gmail.com>
2024-07-29 18:27:11 +0545

    Added login and logout functionalities

--- base/views.py
+++ base/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
@@ -9,6 +12,32 @@ from .forms import RoomForm
 #     {'id': 2, 'name': 'Lets learn Phoenix'},
 #     {'id': 3, 'name': 'Lets learn Web Development'},
 #     ]
+
+def loginPage(request):
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+            pass
+        except:
+            messages.error(request, 'User does not exists.')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exists.')
+
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
