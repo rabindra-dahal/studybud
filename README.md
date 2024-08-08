29c3594a253cdac80a14a36a7abc636072e8303b
Rabindra Dahal <rabindradahal2076@gmail.com>
2024-08-08 15:43:29 +0545

    Added topics and activity page

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # studybud
-  My Study buddy..
+  `My Study buddy..`
+  Reference: `https://www.youtube.com/watch?v=PtQiiknWUcI`
+  To run app: `python manage.py runserver`
+  Browse into browser: `http://localhost:8000/`
--- base/urls.py
+++ base/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('logout/', views.logoutUser, name='logout'),
     path('update-user/', views.updateUser, name='update-user'),
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
+
 
     
 ]
