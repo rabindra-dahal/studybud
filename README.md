1395c07ed42b2b2c561251887a0fad89935a5530
Rabindra Dahal <rabindradahal2076@gmail.com>
2024-08-08 17:42:08 +0545

    Added django restframework

--- README.md
+++ README.md
@@ -9,3 +9,16 @@
 
   
   Browse into browser: `http://localhost:8000/`
+
+
+  REST framework installation:
+
+  ``pip install djangorestframework``
+
+  ``pip install markdown``       # Markdown support for the browsable API.
+  
+  ``pip install django-filter``  # Filtering support
+
+  CORS
+
+  ``pip install django-cors-headers``
--- studybud/settings.py
+++ studybud/settings.py
@@ -38,11 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,3 +132,5 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
